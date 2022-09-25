218c9117e9e62cd4109dc1c9d1e8acc2140ad2d8
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2022-09-25 18:42:10 -0300

    chore: implement whatsapp icon

--- src/components/WhatsApp.tsx
+++ src/components/WhatsApp.tsx
@@ -1,9 +1,14 @@
 import { WhatsappLogo } from 'phosphor-react'
+import Link from 'next/link'
 
 const WhatsApp = () => {
   return (
-    <div className="absolute bottom-0 right-0">
-      <WhatsappLogo size={32} />
+    <div className="fixed bottom-2 right-2 text-white bg-[#39E161] p-2 rounded-lg">
+      <Link href="">
+        <a>
+          <WhatsappLogo size={40} />
+        </a>
+      </Link>
     </div>
   )
 }
